e0870a6c7ccf4a60d292a82ab249647805552724
CamiloCVC <camicastellanos0602@gmail.com>
2022-11-16 18:47:41 -0500

    img fix 1

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Cards.css';
-// import x from '../img/fondoplaceholder.png' 
+import x from '../img/fondoplaceholder.png' 
 
 import Card from './Card.jsx';
 
@@ -22,6 +22,7 @@ export default function Cards({cities, onClose}) {
     else{
       return(
           <div className="img-placeholder">
+            <img src={x} alt="Not Found" />
           </div>
         )}
 }
